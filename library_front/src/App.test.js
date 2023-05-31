c66fd7ae16dc0846569b522c0e1e77530c891880
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-05-30 21:12:31 -0300

    Configuração ci

--- library_front/src/App.test.js
+++ library_front/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Test app', () => {
+  expect(1 + 1).toBe(2);
+});
+
+test('Test app', () => {
+  expect(1 - 1).toBe(0);
+});
+
+test('Test app', () => {
+  expect(2 + 2).toBe(4);
+});
+
+test('Test app', () => {
+  expect(10 + 10).toBe(20);
 });
